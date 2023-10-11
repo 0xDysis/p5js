74391e617f763e40119078e4e2e5526a21a49249
0xDysis <99392905+0xDysis@users.noreply.github.com>
2023-10-11 22:20:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,44 @@
-let maxDepth = 6;
-let zoom = 1;
-let zoomDirection = 1;
+let angle = 0;
+let from;
+let to;
 
 function setup() {
   createCanvas(710, 710);
   noFill();
-  stroke(255);
+  // Define the two colors we want to interpolate between
+  from = color(255, 0, 0);
+  to = color(0, 0, 255);
 }
 
 function draw() {
   clear();
   
   translate(width / 2, height / 2);
+  rotate(angle);
+
+  // Calculate a ratio (between 0 and 1) based on the current frameCount
+  let ratio = abs(sin(frameCount * 0.80));
+  // Interpolate between the two colors using the ratio
+  let currentColor = lerpColor(from, to, ratio);
+  // Set the stroke color to the interpolated color
+  stroke(currentColor);
 
   // Draw the boundary of the Poincaré disk
   ellipse(0, 0, width, height);
 
-  // Apply the zoom effect only to the fractal pattern
-  push();
-  scale(zoom);
-
-  // Draw a fractal pattern of circles within the Poincaré disk
-  drawCircle(0, 0, width / 2, 0);
-
-  pop();
-
-  // Update the zoom effect
-  if (zoom <= 0.1 || zoom >= 1) {
-    zoomDirection *= -1; // Reverse the zoom direction when it reaches the limit
+  // Draw a pattern of circles within the Poincaré disk
+  let numCircles = min(frameCount, 360); // Vary the number of circles up to a maximum of 360
+  for (let i = 0; i < numCircles; i += 10) {
+    let angle = radians(i);
+    let x = cos(angle) * width / 4;
+    let y = sin(angle) * height / 4;
+    let d = dist(0, 0, x, y);
+    let r = sqrt(sq(width / 2 - d)); // Adjust the radius based on the position
+    ellipse(x, y, r * 2);
   }
-  zoom += zoomDirection * 0.01; // Change the zoom level
+
+  // Increase the angle for the next frame
+  angle += 0.20;
 }
 
-// Function to draw a circle and recursively draw smaller circles around it
-function drawCircle(x, y, d, depth) {
-  // Adjust the scale based on the current level of recursion
-  let scalingFactor = pow(0.5, depth / maxDepth);
-  push();
-  scale(scalingFactor);
-  
-  // Stop drawing circles when they would exceed the boundary of the disk at the initial scale
-  if (dist(0, 0, x, y) + d > width / 2) {
-    pop();
-    return;
-  }
 
-  ellipse(x, y, d * 2);
-
-  if (depth < maxDepth) {
-    // Each circle is surrounded by six smaller circles
-    for (let i = 0; i < 6; i++) {
-      let angle = PI / 3 * i;
-      let newX = x + cos(angle) * d * 2 / 3; // Adjust the position based on the hyperbolic geometry
-      let newY = y + sin(angle) * d * 2 / 3; // Adjust the position based on the hyperbolic geometry
-      drawCircle(newX, newY, d / 3, depth + 1);
-    }
-  }
-  
-  pop();
-}
